98cfc3af4a29d8642e08cfb3e2ac5ad6b3b0feaa
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-13 13:13:15 -0500

    Begin development on program's main loop.

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const htmlTemplate = require('./src/htmlTemplate.js')
 
-// Creates 'dist' folder if it does not already exist.
-if (!fs.existsSync('./dist')){
-    fs.mkdirSync('./dist');
+const main = () => {
+    let selection = null;
+
+    inquirer.prompt
+
+    // Creates 'dist' folder if it does not already exist.
+    if (!fs.existsSync('./dist')) {
+        fs.mkdirSync('./dist');
+    }
+
+    // Creates the team dashboard webpage.
+    fs.writeFileSync("./dist/index.html", htmlTemplate.htmlString);
 }
 
-// Creates the team dashboard webpage.
-fs.writeFileSync("./dist/index.html", htmlTemplate.htmlString); 
+// Starts main program loop.
+main();
