b6d8e12687b75d4e4e86c95239a8af15624f9183
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-14 06:33:31 -0500

    Add question loop. TODO: Make selection functions.

--- index.js
+++ index.js
@@ -3,20 +3,98 @@ const fs = require('fs');
 const htmlTemplate = require('./src/htmlTemplate.js');
 
 const main = () => {
-    let selection = null;
-
-    const questions = () => {
-        return inquirer
-            .prompt(
+    const getInfo = (firstTime) => {
+        inquirer
+            .prompt([
                 {
-                    name: '',
+                    name: 'employeeName',
                     type: 'input',
-                    message: 'What is your name? (Manager\s name)'
+                    message: 'What is your name?',
+                    validate: employeeName => {
+                        if (employeeName) {
+                            return true;
+                        } else {
+                            console.log('No input detected.')
+                            return false;
+                        }
+                    }
+                },
+                {
+                    name: 'id',
+                    type: 'input',
+                    message: 'What is your ID number?',
+                    validate: (id) => {
+                        if (!isNaN(id)) {
+                            return true;
+                        } else {
+                            console.log(' - Not a number!');
+                            return false;
+                        }
+                    }
+                },
+                {
+                    name: 'email',
+                    type: 'input',
+                    message: 'What is your email address?',
+                    validate: email => {
+                        if (email) {
+                            return true;
+                        } else {
+                            console.log('No input detected.')
+                            return false;
+                        }
+                    }
+                },
+                {
+                    name: 'office',
+                    type: 'input',
+                    message: 'What is your office number?',
+                    validate: office => {
+                        if (office) {
+                            return true;
+                        } else {
+                            console.log('Invalid ID. Please enter a number.')
+                            return false;
+                        }
+                    }
                 }
-            )
+            ])
+            .then((employeeName, id, email, office) => {
+                if (firstTime === true) {
+                    console.log('True!!!!');
+                    makeManager(employeeName, id, email, office);
+                    return
+                } else {
+                    inquirer
+                        .prompt(
+                            {
+                                name: 'selection',
+                                type: 'list',
+                                message: 'What addition to your team would you like to make?',
+                                choices: ['Engineer', 'Intern', 'Manager']
+                            }
+                        )
+                        .then((selection) => {
+                            if (selection === 'Engineer') {
+                                makeEngineer(employeeName, id, email, office);
+                            }
+                            if (selection === 'Intern') {
+                                makeIntern(employeeName, id, email, office);
+                            }
+                            if (selection === 'Manager') {
+                                makeManager(employeeName, id, email, office);
+                            }
+                        })
+                }
+            });
     }
 
-    
+    // Checks for first use. If yes, then asks for manager information.
+    if (!fs.existsSync('./dist/index.html')) {
+        getInfo(true);
+    } else {
+        getInfo(false);
+    }
 
     // Creates the team dashboard webpage.
     // fs.writeFileSync("./dist/index.html", htmlTemplate.htmlString);
@@ -28,6 +106,4 @@ if (!fs.existsSync('./dist')) {
 }
 
 // Starts main program loop.
-const getResponses = async () => {
-    let responses = await main();
-}
+main();
