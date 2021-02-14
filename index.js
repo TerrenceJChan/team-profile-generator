2442badd04b2dadbe42f5dd9d49bfc4bdb5c3cf4
Terrence Chan <terrencechan.writer@gmail.com>
2021-02-13 21:10:57 -0500

    Add starting prompt.

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const htmlTemplate = require('./src/htmlTemplate.js')
+const htmlTemplate = require('./src/htmlTemplate.js');
 
 const main = () => {
     let selection = null;
 
-    inquirer.prompt
-
-    // Creates 'dist' folder if it does not already exist.
-    if (!fs.existsSync('./dist')) {
-        fs.mkdirSync('./dist');
+    const questions = () => {
+        return inquirer
+            .prompt(
+                {
+                    name: '',
+                    type: 'input',
+                    message: 'What is your name? (Manager\s name)'
+                }
+            )
     }
 
+    
+
     // Creates the team dashboard webpage.
-    fs.writeFileSync("./dist/index.html", htmlTemplate.htmlString);
+    // fs.writeFileSync("./dist/index.html", htmlTemplate.htmlString);
+}
+
+// Creates 'dist' folder if it does not already exist.
+if (!fs.existsSync('./dist')) {
+    fs.mkdirSync('./dist');
 }
 
 // Starts main program loop.
-main();
+const getResponses = async () => {
+    let responses = await main();
+}
